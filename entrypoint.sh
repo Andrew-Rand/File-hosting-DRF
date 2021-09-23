2f421fd150a0e660ab6e402ba0813a80b3cbfbc8
andrey <1234567>
2021-09-23 17:24:29 +0300

    configure nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ python manage.py makemigrations
 
 python manage.py migrate
 
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --no-input
+
+exec gunicorn src.config.wsgi:application -b 0.0.0.0:8000 --reload
--- src/config/settings.py
+++ src/config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '/static/'
