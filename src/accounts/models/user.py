bbbb42c8f92dbbc4983baddbee880431a49c61d0
andrey <1234567>
2021-09-24 17:37:13 +0300

    Add BaseModel, Usermodel and manager for correct ath as superuser

--- src/accounts/models/user.py
+++ src/accounts/models/user.py
@@ -1,18 +1,24 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from src.accounts.managers import MyUserManager
 from src.basecore.base_model import BaseModel
 
 
-class User(AbstractBaseUser, BaseModel):
+class User(AbstractUser, BaseModel):
     username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     first_name = models.CharField(verbose_name='First name', max_length=50)
     last_name = models.CharField(verbose_name='Last name', max_length=50)
     email = models.EmailField(unique=True,
                               verbose_name='Email',
                               max_length=100)
-    age = models.PositiveIntegerField(verbose_name='Age')
-    password = models.CharField(verbose_name='Password', max_length=30)
+    age = models.PositiveIntegerField(verbose_name='Age', null=True)
+    password = models.CharField(verbose_name='Password', max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    objects = MyUserManager()
