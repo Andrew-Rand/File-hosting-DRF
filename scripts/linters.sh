f61f4f8fdbd0d2c4f1cb9f07172070bb278ae962
andrey <1234567>
2021-10-04 19:54:35 +0300

    fix linter and clear code

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -2,7 +2,7 @@
 
 echo ">>> $(basename ${BASH_SOURCE[0]})"
 
-#set -o errexit # exit script when command fails
+set -o errexit # exit script when command fails
 set -o pipefail # this setting prevents errors in a pipeline from being masked
 set -o nounset # exit script when it tries to use undeclared variables
 
