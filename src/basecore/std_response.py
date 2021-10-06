013d189e87925975984ca6444d696c87aaf108d4
Your Name <you@example.com>
2021-10-06 10:01:30 +0300

    Add error handler with info

--- src/basecore/std_response.py
+++ src/basecore/std_response.py
@@ -1,7 +1,8 @@
-def create_std_response(result: dict = None, error_code: int = None, total_count: int = 0) -> dict:
+def create_std_response(result: dict = None, error_code: int = None, info: str = None, total_count: int = 0) -> dict:
     return {
         'data': {
             'error': error_code,
+            'info': info,
             'result': result,
             'total_count': total_count
         }
