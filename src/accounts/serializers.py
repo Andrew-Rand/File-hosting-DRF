b686dbeececad52b015e0b7b600dd1636492a762
andrey <1234567>
2021-10-07 16:51:08 +0300

    add working std authenticate and password hashing

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -13,10 +13,17 @@ class UserSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}  # hide password in response
         }
 
+    def create(self, validated_data:Dict[str, Any]) -> User:
+        user = User.objects.create_user(**validated_data)
+
+        return user
+
 
 class UserLoginSerializer(serializers.ModelSerializer):
+
     username = serializers.CharField(max_length=50)
     password = serializers.CharField(max_length=100, write_only=True)
+    email = serializers.EmailField(max_length=100)
 
     class Meta:
         model = User
@@ -29,16 +36,16 @@ class UserLoginSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'An email address is required to log in.'
             )
-
         if password is None:
             raise serializers.ValidationError(
                 'A password is required to log in.'
             )
         user = authenticate(username=username, password=password)
         if user is None:
-            raise serializers.ValidationError(f"User not found")
+            raise serializers.ValidationError(f"User with this username not found or password was incorrect")
         return {
             "id": user.id,
+            "username": user.username,
             "email": user.email
             }
 
--- src/accounts/views/login_view.py
+++ src/accounts/views/login_view.py
@@ -13,8 +13,8 @@ class LoginView(generics.GenericAPIView):
 
     def post(self, request: Request) -> Response:
         serializer = UserLoginSerializer(data=request.data)
-        if not serializer.is_valid(raise_exception=False):
-            raise serializers.ValidationError('Auth f**k')
+        if not serializer.is_valid(raise_exception=True):
+            raise serializers.ValidationError('Authentication failed')
         user_id = serializer.data.get('id')
         access_token = create_token(user_id, time_delta_days=1)
         refresh_token = create_token(user_id, time_delta_days=30)
