9a5a11c40ca4a1d51c87ecdba38ff2f44b39ba64
andrey <1234567>
2021-10-08 17:44:52 +0300

    fix auth with response

--- src/accounts/views/auth_view.py
+++ src/accounts/views/auth_view.py
@@ -5,11 +5,12 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from src.accounts.authentication import login_required
-from src.basecore.std_response import create_std_response
+from src.basecore.responses import OkResponse
 
 
 class AuthView(generics.GenericAPIView):
 
     @login_required
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        return Response(create_std_response(result={"is login?": "yes"}))
+        result_to_response = {"is login?": "yes"}
+        return OkResponse(data=result_to_response)
