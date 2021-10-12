9bda2172f75f5341e1ecad919cb210d15e79ad5a
andrey <1234567>
2021-10-12 16:50:00 +0300

    increase readability in create_std_response func

--- src/basecore/std_response.py
+++ src/basecore/std_response.py
@@ -1,7 +1,12 @@
 from typing import Dict, Any, Tuple
 
 
-def create_std_response(result: Dict[str, Any] = None, status_code: int = None, error_detail: Tuple[Any, ...] = None, total_count: int = 0) -> Dict[str, Any]:
+def create_std_response(
+        result: Dict[str, Any] = None,
+        status_code: int = None,
+        error_detail: Tuple[Any, ...] = None,
+        total_count: int = 0,
+        ) -> Dict[str, Any]:
     return {
         'data': {
             'status_code': status_code,
