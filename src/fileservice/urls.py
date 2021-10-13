b902a3ea6a3c7fa36fb1ebfeda3acea8972651ff
andrey <1234567>
2021-10-13 17:47:07 +0300

    create view for upload chunck files

--- src/fileservice/urls.py
+++ src/fileservice/urls.py
@@ -4,6 +4,6 @@ from .views.file_upload_view import FileUploadView
 from .views.api_upload import ApiUploadView
 
 urlpatterns = [
-    re_path(r'^api/upload/$', ApiUploadView, name="api_file_upload"),
+    re_path(r'^api/upload/$', ApiUploadView.as_view(), name="api_file_upload"),
     re_path(r'^upload/$', FileUploadView.as_view(), name="file_upload"),
 ]
--- src/fileservice/views/api_upload.py
+++ src/fileservice/views/api_upload.py
@@ -5,13 +5,9 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from src.basecore.responses import OkResponse
-
 
 class ApiUploadView(generics.GenericAPIView):
     renderer_classes = (TemplateHTMLRenderer, )
 
     def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        # response_data = {"Do you see any files?": "I also don`t see, but files are here!"}
-        # return OkResponse(data=response_data)
         return Response(template_name="index.html")
