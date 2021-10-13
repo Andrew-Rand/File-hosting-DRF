cbf31d60ccf9c67d84d551eb417e257fc9ac2326
andrey <1234567>
2021-10-13 17:47:07 +0300

    create view for upload chunck files

--- src/fileservice/views/file_upload_view.py
+++ src/fileservice/views/file_upload_view.py
@@ -1,66 +1,63 @@
 import os
 from typing import Any
 
+from django.http import HttpResponse
 from rest_framework import generics
-from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.request import Request
-from rest_framework.response import Response
-
-from src.basecore.custom_error_handler import BadRequestError, NotFoundError
-from src.basecore.responses import OkResponse
 
 
-def get_chunk_name(uploaded_filename, chunk_number):
-    return uploaded_filename + "_part_%03d" % chunk_number
+def get_chunk_name(uploaded_filename: str, chunk_number: int) -> str:
+    return uploaded_filename + f"_part_{chunk_number}"
 
 
 class FileUploadView(generics.GenericAPIView):
 
     TempBase = os.path.expanduser("home/tmp/uploads")
 
-    def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        resumable_identfier = request.data.get('resumableIdentifier', type=str)
-        resumable_filename = request.data.get('resumableFilename', type=str)
-        resumable_chunk_number = request.data.get('resumableChunkNumber', type=int)
+    def get(self, request: Request, *args: Any, **kwargs: Any) -> HttpResponse:
+        resumable_identifier = request.query_params.get('resumableIdentifier')
+        resumable_filename = request.query_params.get('resumableFilename')
+        resumable_chunk_number = int(request.query_params.get('resumableChunkNumber'))
 
-        if not resumable_identfier or not resumable_filename or not resumable_chunk_number:
+        if not resumable_identifier or not resumable_filename or not resumable_chunk_number:
             # Parameters are missing or invalid
-            BadRequestError('Parameter error')
+            # BadRequestError('Parameter error')
+            return HttpResponse(500, "parameter error")
 
-        # chunk folder path based on the parameters
-        temp_dir = os.path.join(FileUploadView.TempBase, resumable_identfier)
+        temp_dir = os.path.join(FileUploadView.TempBase, resumable_identifier)
 
-        # chunk path based on the parameters
         chunk_file = os.path.join(temp_dir, get_chunk_name(resumable_filename, resumable_chunk_number))
-        print(f'Getting chunk: {chunk_file}')
+        print(f'Getchunk: {chunk_file}')
 
         if os.path.isfile(chunk_file):
             # Let resumable.js know this chunk already exists
-            response_data = {"File upload": "Yes"}
-            return OkResponse(data=response_data)
+            return HttpResponse(200, "OK")
         else:
             # Let resumable.js know this chunk does not exists and needs to be uploaded
-            raise NotFoundError('Not found')
+            return HttpResponse(400, "not found")
 
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> HttpResponse:
+        resumable_total_chunks = int(request.data.get('resumableTotalChunks'))
+        resumable_chunk_number = int(request.data.get('resumableChunkNumber'))
+        resumable_filename = request.data.get('resumableFilename')
+        resumable_identifier = request.data.get('resumableIdentifier')
 
-    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        resumable_total_chunks = request.form.get('resumableTotalChunks', type=int)
-        resumable_chunk_number = request.form.get('resumableChunkNumber', default=1, type=int)
-        resumable_filename = request.form.get('resumableFilename', default='error', type=str)
-        resumable_identfier = request.form.get('resumableIdentifier', default='error', type=str)
+        # get chunk data
+        chunk_data = request.FILES.get('file')
 
-        # get the chunk data
-        chunk_data = request.files['file']
-
-        # make our temp directory
-        temp_dir = os.path.join(FileUploadView.TempBase, resumable_identfier)
+        # make temp directory
+        temp_dir = os.path.join(FileUploadView.TempBase, resumable_identifier)
         if not os.path.isdir(temp_dir):
             os.makedirs(temp_dir, 0o777)
 
-        # save the chunk data
+        # save chunk data
         chunk_name = get_chunk_name(resumable_filename, resumable_chunk_number)
         chunk_file = os.path.join(temp_dir, chunk_name)
-        chunk_data.save(chunk_file)
+
+        with open(chunk_file, "wb") as file:
+            for chunk in chunk_data.chunks():
+                file.write(chunk)
+
         print(f'Saved chunk: {chunk_file}')
 
         # check if the upload is complete
@@ -82,5 +79,4 @@ class FileUploadView(generics.GenericAPIView):
             os.rmdir(temp_dir)
             print(f'File saved to: {target_file_name}')
 
-        response_data = {"File saved to": target_file}
-        return OkResponse(data=response_data)
+        return HttpResponse(200, "File saved to: {target_file}")
