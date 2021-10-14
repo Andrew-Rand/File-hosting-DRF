a01b3481ac025a235c66acd51748fa4ce8239f3a
Your Name <you@example.com>
2021-10-14 14:17:38 +0300

    change urls and view name

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('src.accounts.urls')),
-    path('files/', include('src.fileservice.urls')),
+    path('api/accounts', include('src.accounts.urls')),
+    path('api/files/', include('src.fileservice.urls')),
 ]
