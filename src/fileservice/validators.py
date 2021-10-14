32d82d7109a6858d7a8492e3e4c415a76d5070fb
Your Name <you@example.com>
2021-10-14 13:17:44 +0300

    simplified the validator with python-magic

--- src/fileservice/validators.py
+++ src/fileservice/validators.py
@@ -1,22 +1,11 @@
 from typing import Any
+import magic
 
-
-CORRECT_SIGNATURE = {
-    "jpg": {
-        "signature": ("FF D8 FF E0", "FF D8 FF E1", "FF D8 FF E2", "FF D8 FF E8"),
-        "offset_signature": 0  # смещение сигнатуры относительно начала файла
-    }
-}
+ALLOWED_FILETYPES = ('image/jpeg', 'image/png', 'application/pdf', 'text/plain', 'video/webm', )
 
 
 def validate_typefile(value: Any) -> bool:
-    file_to_validate = value.read(32)
-    hex_file = " ".join(['{:02X}'.format(byte) for byte in file_to_validate])
-
-    validation_res = []
-    for key, value in CORRECT_SIGNATURE.items():
-        for signature in value.get('signature'):
-            offset = value.get('offset_signature') * 2 + value.get('offset_signature')  # 2chars for bytes + number of whitespaces between bytes
-            if signature == hex_file[offset:len(signature) + offset].upper():
-                validation_res.append(key)
-    return not validation_res == []
+    file = value.read(2048)
+    file_type = magic.from_buffer(file, mime=True)
+    print(file_type)
+    return file_type in ALLOWED_FILETYPES
