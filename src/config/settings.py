fc19e2b90ffb15ff84aab7be2d8576b271f86124
andrey <1234567>
2021-10-15 17:44:59 +0300

    fix model fields

--- src/config/settings.py
+++ src/config/settings.py
@@ -107,9 +107,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 AUTH_USER_MODEL = 'accounts.User'
 
 REST_FRAMEWORK = {
