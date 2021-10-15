5a42df71987e5bad71f904e8adf22e716bb80ecb
andrey <1234567>
2021-10-15 17:49:08 +0300

    Change tmp to transitional

--- src/fileservice/models/file_storage.py
+++ src/fileservice/models/file_storage.py
@@ -3,15 +3,12 @@ from django.db import models
 from src.basecore.base_model import BaseModel
 
 
-TEMP_DIR = 'temp'
-PERMANENT_DIR = 'permanent'
-
 STORAGE_TYPE_CHOICES = (
-    ("tmp", 'temp_storage'),
-    ("prm", 'permanent'),
+    ("trans", 'transitional'),
+    ("perm", 'permanent'),
 )
 
 
 class FileStorage(BaseModel):
-    type = models.CharField(choices=STORAGE_TYPE_CHOICES, default="tmp")
+    type = models.CharField(choices=STORAGE_TYPE_CHOICES, default="trans")
     destination = models.CharField(max_length=256)
