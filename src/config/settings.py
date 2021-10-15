1c35718e467e2fbceb8209b88a65c376d5d02688
Your Name <you@example.com>
2021-10-15 13:16:36 +0300

    change-models

--- src/config/settings.py
+++ src/config/settings.py
@@ -105,8 +105,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'accounts.User'
 
--- src/fileservice/models/files.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from src.accounts.models import User
-from src.basecore.base_model import BaseModel
-
-
-class File(BaseModel):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="files", null=True)
-    file = models.FileField(upload_to="home/tmp/uploads")
