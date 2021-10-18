9fc6a4f8092737115a38366080f0747e16b9a2d9
andrey <1234567>
2021-10-18 16:52:21 +0300

    rm required=True in serializer fields

--- src/fileservice/serializers/upload_data_serializer.py
+++ src/fileservice/serializers/upload_data_serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 
 class UploadDataSerializer(serializers.Serializer):
-    resumable_total_chunks = serializers.IntegerField(required=True, read_only=True, min_value=1)
-    resumable_chunk_number = serializers.IntegerField(required=True, read_only=True, min_value=1)
-    resumable_filename = serializers.CharField(max_length=256, required=True, read_only=True)
-    resumable_identifier = serializers.CharField(max_length=256, required=True, read_only=True)
+    resumable_total_chunks = serializers.IntegerField(read_only=True, min_value=1)
+    resumable_chunk_number = serializers.IntegerField(read_only=True, min_value=1)
+    resumable_filename = serializers.CharField(max_length=256, read_only=True)
+    resumable_identifier = serializers.CharField(max_length=256, read_only=True)
