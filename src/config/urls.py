bbcbf9e081a6d42ea58f0a0a09817d93b8c7f113
andrey <1234567>
2021-10-18 17:28:32 +0300

    create new endpoint with file_build in different view

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts', include('src.accounts.urls')),
+    path('api/accounts/', include('src.accounts.urls')),
     path('api/files/', include('src.fileservice.urls')),
 ]
---
+++ src/fileservice/views/file_build_view.py
@@ -0,0 +1,42 @@
+import os
+from typing import Any
+
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from src.basecore.responses import OkResponse
+from src.fileservice.views.file_upload_view import get_chunk_name
+
+
+class FileBuildView(generics.GenericAPIView):
+
+    TempBase = os.path.expanduser("home/tmp/uploads")
+
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        total_chunks = int(request.data.get('resumableTotalChunks'))
+        filename = request.data.get('resumableFilename')
+        identifier = request.data.get('resumableIdentifier')
+
+        # make temp directory
+        temp_dir = os.path.join(FileBuildView.TempBase, identifier)
+
+        # check if the upload is complete
+        chunk_paths = [os.path.join(temp_dir, get_chunk_name(filename, x)) for x in
+                       range(1, total_chunks + 1)]
+        upload_complete = all([os.path.exists(p) for p in chunk_paths])
+
+        # create final file from all chunks
+        if upload_complete:
+            target_file_name = os.path.join(FileBuildView.TempBase, filename)
+            with open(target_file_name, "ab") as target_file:
+                for p in chunk_paths:
+                    stored_chunk_file_name = p
+                    stored_chunk_file = open(stored_chunk_file_name, 'rb')
+                    target_file.write(stored_chunk_file.read())
+                    stored_chunk_file.close()
+                    os.unlink(stored_chunk_file_name)
+            target_file.close()
+            os.rmdir(temp_dir)
+
+        return OkResponse(data={"ok?": "ok"})
