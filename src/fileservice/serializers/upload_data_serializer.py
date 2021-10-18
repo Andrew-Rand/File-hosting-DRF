941db8c217eb13613f7b652fafa8a9b5f6e83194
andrey <1234567>
2021-10-18 16:50:19 +0300

    Add read_only in serializer fields

--- src/fileservice/serializers/upload_data_serializer.py
+++ src/fileservice/serializers/upload_data_serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 
 class UploadDataSerializer(serializers.Serializer):
-    resumable_total_chunks = serializers.IntegerField(required=True, min_value=1)
-    resumable_chunk_number = serializers.IntegerField(required=True, min_value=1)
-    resumable_filename = serializers.CharField(max_length=256, required=True)
-    resumable_identifier = serializers.CharField(max_length=256, required=True)
+    resumable_total_chunks = serializers.IntegerField(required=True, read_only=True, min_value=1)
+    resumable_chunk_number = serializers.IntegerField(required=True, read_only=True, min_value=1)
+    resumable_filename = serializers.CharField(max_length=256, required=True, read_only=True)
+    resumable_identifier = serializers.CharField(max_length=256, required=True, read_only=True)
