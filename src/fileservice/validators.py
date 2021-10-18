ebdeb286c9d980357696b99987c6b6194d4a8943
andrey <1234567>
2021-10-18 19:22:39 +0300

    add GIF type into set

--- src/fileservice/validators.py
+++ src/fileservice/validators.py
@@ -14,7 +14,7 @@ DOC = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 XML = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 TXT = 'text/plain'
 
-ALLOWED_FILETYPES = {JPG, PNG, PDF, SVG, TIF, TXT, DOC, XML}
+ALLOWED_FILETYPES = {JPG, PNG, GIF, PDF, SVG, TIF, TXT, DOC, XML}
 
 
 def is_file_type_supported(file: Union[UploadedFile, pathlib.Path]) -> bool:
