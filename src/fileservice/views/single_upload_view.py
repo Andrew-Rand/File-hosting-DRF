25f418681073dfcab594afd88be0ac9fb53c4d9f
andrey <1234567>
2021-10-19 19:50:35 +0300

    change TempBase in snake case

--- src/fileservice/views/single_upload_view.py
+++ src/fileservice/views/single_upload_view.py
@@ -10,7 +10,7 @@ from src.basecore.responses import OkResponse
 
 class SingleUploadView(generics.GenericAPIView):
 
-    TempBase = os.path.expanduser('home/tmp/uploads')
+    temp_base = os.path.expanduser('home/tmp/uploads')
 
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
 
@@ -18,11 +18,11 @@ class SingleUploadView(generics.GenericAPIView):
         file_data = request.FILES.get('file')
 
         # make directory
-        temp_dir = os.path.join(SingleUploadView.TempBase, file_name)
+        temp_dir = os.path.join(SingleUploadView.temp_base, file_name)
         if not os.path.isdir(temp_dir):
             os.makedirs(temp_dir, 0o777)
 
-        saved_file = os.path.join(SingleUploadView.TempBase, file_data)
+        saved_file = os.path.join(SingleUploadView.temp_base, file_data)
 
         with open(saved_file, "wb") as file:
             for row in file_data.chunks():
