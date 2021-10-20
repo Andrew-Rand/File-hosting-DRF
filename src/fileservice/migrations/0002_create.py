4276b0551e732d2ce3215b10b46a2b7dfaf66afc
Your Name <you@example.com>
2021-10-20 12:59:01 +0300

    create migration for running to objects into FileStorages db

---
+++ src/fileservice/migrations/0002_create.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+
+def load_data(apps, shema_editor):
+    FileStorage = apps.get_model('files', 'file_storages')
+
+    FileStorage(type='temp', destination='storage/temp/').save()
+    FileStorage(type='permanent', destination='storage/permanent/').save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('files', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_data)
+    ]
