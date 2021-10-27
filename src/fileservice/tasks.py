4ca9de0eb96bea4f42255de12cc2dab851d0bd3b
Your Name <you@example.com>
2021-10-27 14:50:47 +0300

    change @shared_task to @app.taks

--- src/fileservice/tasks.py
+++ src/fileservice/tasks.py
@@ -1,16 +1,17 @@
 from celery import shared_task
 
 from src.accounts.models import User
+from src.config.celery import app
 from src.fileservice.models import FileStorage
 from src.fileservice.serializers.file_upload_parameters_serializer import FileUploadParametersSerializer
 from src.fileservice.utils import build_file_from_chunks
 
 
-@shared_task
+@app.task
 def task_build_file(user: User, temp_storage: FileStorage, permanent_storage: FileStorage, serializer: FileUploadParametersSerializer) -> None:
     build_file_from_chunks(user, temp_storage, permanent_storage, serializer)
 
 
-@shared_task
+@app.task
 def test_task() -> str:
     return "Your celery task is working"
