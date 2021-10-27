42c92338c611e40bc684ec5f4fe6c9fe034d5d67
Your Name <you@example.com>
2021-10-27 13:57:49 +0300

    change celery time zone to UTC

--- src/config/celery_config.py
+++ src/config/celery_config.py
@@ -1,7 +1,7 @@
 from decouple import config
 
 
-timezone = 'Europe/Minsk'
+timezone = 'UTC'
 task_track_started = True
 task_time_limit = 30 * 60
 broker_url = config('CELERY_BROKER_HOST_AND_PORT')
