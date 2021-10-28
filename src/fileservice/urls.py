bf3c47ffda84f893f75b950e6b0620def0667c04
Your Name <you@example.com>
2021-10-28 11:50:57 +0300

    create view for download file

--- src/fileservice/urls.py
+++ src/fileservice/urls.py
@@ -2,10 +2,13 @@ from django.urls import re_path
 
 from .views.file_build_view import FileBuildView
 from .views.chunk_upload_view import ChunkUploadView
+from .views.file_download_view import FileDownloadView
 from .views.upload_template_view import UploadTemplateView
 
 urlpatterns = [
     re_path(r'^template/$', UploadTemplateView.as_view(), name='api_file_upload'),
     re_path(r'^upload/$', ChunkUploadView.as_view(), name='file_upload'),
-    re_path(r'^build/$', FileBuildView.as_view(), name='file_build')
+    re_path(r'^build/$', FileBuildView.as_view(), name='file_build'),
+
+    re_path(r'^download/$', FileDownloadView().as_view(), name='file_download'),
 ]
