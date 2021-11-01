3a81e9d5afac76ab56b78f47b13d06383136c437
Your Name <you@example.com>
2021-11-01 09:06:34 +0300

    add logger support in celery task

--- src/fileservice/tasks.py
+++ src/fileservice/tasks.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from typing import Dict, Any
 
 from src.accounts.models import User
+from src.basecore import logger_conf
 from src.etl import celery_app
 from src.fileservice.models import FileStorage, File
 from src.fileservice.models.file_storage import TEMP_STORAGE
@@ -11,9 +12,13 @@ from src.fileservice.utils import is_all_chunk_uploaded, save_file, calculate_ha
 
 CHUNK_EXPIRATION_TIME = timedelta(days=7)
 
+logger = logger_conf.get_logger(__name__)
+
 
 @celery_app.task
 def task_build_file(user_id: str, temp_storage_id: str, permanent_storage_id: str, data: Dict[str, Any]) -> None:
+
+    logger.info('Celery task for filebuild starts')  # info only in console
 
     user = User.objects.get(id=user_id)
     permanent_storage = FileStorage.objects.get(id=permanent_storage_id)
@@ -25,7 +30,9 @@ def task_build_file(user_id: str, temp_storage_id: str, permanent_storage_id: st
     chunk_paths = make_chunk_paths(chunks_dir_path, data)
     if not is_all_chunk_uploaded(chunk_paths):
         send_warning_email_to_user(user.email, 'The server cant build your file, all chunks are not loaded!')
-        raise Exception
+        logger.info('There is a problem with filebuild (all chunks was not uploaded)')
+        logger.warning(f'File was not build, warning email send to user {user.email}')  # whrite in console and in logfile
+        raise FileExistsError
 
     # create final file from all chunks
     user_storage_path = os.path.join(permanent_storage.destination, str(user.id))
@@ -37,6 +44,8 @@ def task_build_file(user_id: str, temp_storage_id: str, permanent_storage_id: st
     file_hash = calculate_hash_md5(target_file_path)
 
     File.create_model_object(user, file_hash, permanent_storage, target_file_path, data)
+
+    logger.info('Celery task for filebuild сompleted successfully')
 
 
 @celery_app.task
