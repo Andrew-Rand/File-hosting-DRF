152dddbd3b1d34807f729e8ae2f8246969eaf793
andrey <1234567>
2021-11-01 19:11:34 +0300

    add view for file detail

--- src/appservice/urls.py
+++ src/appservice/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
 from .views.all_files_view import AllFilesView
-
+from .views.file_detail_view import FileDetailView
 
 urlpatterns = [
     re_path(r'^files/$', AllFilesView.as_view(), name='all_user_files'),
+    re_path(r'^files/(?P<pk>.+)/$', FileDetailView.as_view(), name='file_detail'),
 ]
