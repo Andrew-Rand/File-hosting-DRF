0d5304cc3346856ed6c067525f39182c84af4ce2
andrey <1234567>
2021-11-01 20:01:01 +0300

    add try-except

--- src/appservice/serializers/file_delete_serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from src.fileservice.models import File
-
-
-class FileDeleteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = File
-        fields = ('is_alive', )
