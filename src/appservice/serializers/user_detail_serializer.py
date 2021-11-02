4b7251feae138c090e79a7cfa0450fbe7885e285
Your Name <you@example.com>
2021-11-02 08:04:30 +0300

    create user-detail-view (get)

---
+++ src/appservice/serializers/user_detail_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from src.accounts.models import User
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'age')
--- src/appservice/urls.py
+++ src/appservice/urls.py
@@ -3,9 +3,12 @@ from django.urls import re_path
 from .views.all_files_view import AllFilesView
 from .views.delete_file_view import DeleteFileView
 from .views.file_detail_view import FileDetailView
+from .views.user_detail_view import UserDetailView
 
 urlpatterns = [
     re_path(r'^files/$', AllFilesView.as_view(), name='all_user_files'),
     re_path(r'^files/(?P<pk>.+)/$', FileDetailView.as_view(), name='file_detail'),
-    re_path(r'delete/(?P<pk>.+)/$', DeleteFileView.as_view(), name='delete_file')
+    re_path(r'delete/(?P<pk>.+)/$', DeleteFileView.as_view(), name='delete_file'),
+    re_path(r'user/$', UserDetailView.as_view(), name='user_view_and_update'),
+
 ]
