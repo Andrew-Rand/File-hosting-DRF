a89b2a3975698fe23457cd13aea79b24972eafee
Your Name <you@example.com>
2021-11-02 12:59:55 +0300

    add Change password view

--- src/appservice/urls.py
+++ src/appservice/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
 
 from .views.all_files_view import AllFilesView
+from .views.change_password_view import ChangePasswordView
 from .views.delete_file_view import DeleteFileView
 from .views.file_detail_view import FileDetailView
 from .views.user_detail_view import UserDetailView
@@ -10,5 +11,6 @@ urlpatterns = [
     re_path(r'^files/(?P<pk>.+)/$', FileDetailView.as_view(), name='file_detail'),
     re_path(r'delete/(?P<pk>.+)/$', DeleteFileView.as_view(), name='delete_file'),
     re_path(r'user/$', UserDetailView.as_view(), name='user_view_and_update'),
+    re_path(r'user/change_password/$', ChangePasswordView.as_view(), name='change_password'),
 
 ]
