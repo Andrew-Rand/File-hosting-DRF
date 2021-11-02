7999ccd6bfdfd02e8e6eee92b18a42d39371eca5
Your Name <you@example.com>
2021-11-02 08:04:30 +0300

    create user-detail-view (get)

---
+++ src/appservice/views/user_detail_view.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from src.accounts.authentication import login_required
+from src.accounts.models import User
+from src.appservice.serializers.user_detail_serializer import UserDetailSerializer
+from src.basecore.custom_error_handler import NotFoundError
+from src.basecore.responses import OkResponse
+
+
+class UserDetailView(generics.GenericAPIView):
+
+    @login_required
+    def get(self, request: Request, *args: Any, user: User, **kwargs: Any) -> Response:
+        try:
+            user = User.objects.get(id=user.id)
+        except User.DoesNotExist:
+            raise NotFoundError('This user does not exist')
+        serializer_for_queryset = UserDetailSerializer(instance=user)
+        return Response(serializer_for_queryset.data)
+
+    # @login_required
+    # def put(self, request: Request, *args: Any, user: User, **kwargs: Any) -> Response:
+    #     try:
+    #         user = User.objects.get(id=user.id)
+    #     except User.DoesNotExist:
+    #         raise NotFoundError('This user does not exist')
+    #     serializer = UserDetailSerializer(data=request.data)
+    #     user.save()
+    #     return OkResponse(data=serializer.data)
