cfe3b7caf625236ef2866455953f8e99fdc8e3bf
andrey <1234567>
2021-11-03 19:14:56 +0300

    fix all commetns

--- src/fileservice/views/file_download_view.py
+++ src/fileservice/views/file_download_view.py
@@ -1,16 +1,12 @@
-import os
 from typing import Any
 
-from django.http import HttpResponse
 from rest_framework import generics
-from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from src.accounts.authentication import login_required
 from src.accounts.models import User
-from src.basecore.custom_error_handler import BadRequestError, NotFoundError
-from src.basecore.responses import OkResponse
+from src.basecore.custom_error_handler import NotFoundError
 from src.fileservice.models import File
 
 
@@ -19,13 +15,10 @@ class FileDownloadView(generics.GenericAPIView):
     @login_required
     def get(self, request: Request, *args: Any, user: User, **kwargs: Any) -> Response:
 
-        try:
-            file = File.objects.get(id=self.kwargs['pk'])
-        except File.DoesNotExist:
-            raise NotFoundError('This file does not exist')
+        if not File.objects.filter(id=self.kwargs['pk'], user=user).exists():
+            raise NotFoundError('This file does not exist or doesn`t belong to this user')
 
-        if not file.user.id == user.id:
-            raise BadRequestError('This user doesn`t  have this file in own repository')
+        file = File.objects.get(id=self.kwargs['pk'])
 
         file_path = file.get_absolute_path()
 
