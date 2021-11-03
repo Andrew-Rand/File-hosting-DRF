d6c285170584c03ae7f30184be1cb0eefa14429f
Your Name <you@example.com>
2021-11-03 08:51:37 +0300

    fix uuid re_path in url

--- src/fileservice/urls.py
+++ src/fileservice/urls.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
 
+from .views.all_download_view import AllDownloadView
 from .views.file_build_view import FileBuildView
 from .views.chunk_upload_view import ChunkUploadView
+from .views.file_download_view import FileDownloadView
 from .views.file_upload_view import FileUploadView
 from .views.upload_template_view import UploadTemplateView
 
@@ -11,4 +13,6 @@ urlpatterns = [
     re_path(r'^chunk-upload/$', ChunkUploadView.as_view(), name='chunk_upload'),
     re_path(r'^build/$', FileBuildView.as_view(), name='file_build'),
     re_path(r'^file-upload/$', FileUploadView.as_view(), name='file_upload'),
+    re_path(r'^(?P<pk>[0-9A-Fa-f-]+)/download$', FileDownloadView.as_view(), name='file_download'),
+    re_path(r'^download/$', AllDownloadView.as_view(), name='all_download_zip'),
 ]
