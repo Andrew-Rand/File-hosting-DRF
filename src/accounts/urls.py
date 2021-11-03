b921a55de5a0e004c3688e3e6e102a920f32cda5
andrey <1234567>
2021-11-03 19:57:35 +0300

    add appservice endpoints in fileservice and accounts

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,13 +1,15 @@
 from django.urls import re_path
 
-from .views.auth_view import AuthView
+from .views.change_password_view import ChangePasswordView
 from .views.login_view import LoginView
 from .views.refresh_view import RefreshView
 from .views.register_view import RegisterView
+from .views.user_detail_view import UserDetailView
 
 urlpatterns = [
-    re_path(r'^/$', AuthView.as_view(), name="test_login"),
     re_path(r'^register/$', RegisterView.as_view(), name="register"),
     re_path(r'^login/$', LoginView.as_view(), name="login"),
     re_path(r'^refresh/$', RefreshView.as_view(), name="refresh"),
+    re_path(r'^/$', UserDetailView.as_view(), name='user_view_and_update'),
+    re_path(r'^change_password/$', ChangePasswordView.as_view(), name='change_password'),
 ]
--- src/fileservice/urls.py
+++ src/fileservice/urls.py
@@ -1,13 +1,22 @@
 from django.urls import re_path
 
+from .views.all_files_view import AllFilesView
 from .views.file_build_view import FileBuildView
 from .views.chunk_upload_view import ChunkUploadView
+from .views.file_delete_view import DeleteFileView
+from .views.file_detail_view import FileDetailView
+from .views.file_edit_description_view import EditFiledescriptionView
 from .views.file_upload_view import FileUploadView
 from .views.upload_template_view import UploadTemplateView
+
 
 urlpatterns = [
     re_path(r'^template/$', UploadTemplateView.as_view(), name='api_file_upload'),
     re_path(r'^chunk-upload/$', ChunkUploadView.as_view(), name='chunk_upload'),
     re_path(r'^build/$', FileBuildView.as_view(), name='file_build'),
     re_path(r'^file-upload/$', FileUploadView.as_view(), name='file_upload'),
+    re_path(r'^/$', AllFilesView.as_view(), name='all_user_files'),
+    re_path(r'^(?P<pk>[0-9A-Fa-f-]+)/$', FileDetailView.as_view(), name='file_detail'),
+    re_path(r'^(?P<pk>[0-9A-Fa-f-]+)/delete/$', DeleteFileView.as_view(), name='delete_file'),
+    re_path(r'^(?P<pk>[0-9A-Fa-f-]+)/edit/$', EditFiledescriptionView.as_view(), name='edit_description'),
 ]
