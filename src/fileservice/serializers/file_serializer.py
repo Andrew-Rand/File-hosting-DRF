699fd926e1f5c2b6221c832cb9e3885d0415a002
andrey <1234567>
2021-11-04 17:44:02 +0300

    add pagination and ordering in file list view

--- src/fileservice/serializers/file_serializer.py
+++ src/fileservice/serializers/file_serializer.py
@@ -6,4 +6,4 @@ from src.fileservice.models import File
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
-        fields = ('id', 'name', 'type', 'date_created', 'is_alive', 'description')
+        fields = ('id', 'name', 'type', 'date_created', 'date_modified', 'is_alive', 'description')
--- src/fileservice/views/file_list_view.py
+++ src/fileservice/views/file_list_view.py
@@ -8,9 +8,13 @@ from src.accounts.authentication import login_required
 from src.accounts.models import User
 from src.fileservice.serializers.file_serializer import FileSerializer
 from src.fileservice.models import File
+from src.fileservice.views.constants import PAGINATION_NUMBER, ORDERING_FILED
 
 
 class FileListView(generics.GenericAPIView):
+
+    paginate_by = PAGINATION_NUMBER
+    ordering = ORDERING_FILED
 
     @login_required
     def get(self, request: Request, *args: Any, user: User, **kwargs: Any) -> Response:
