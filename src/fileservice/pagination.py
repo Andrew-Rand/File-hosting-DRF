6e4dce2612e3c64076ce4e3a821ced8d9e686aaf
Your Name <you@example.com>
2021-11-05 09:07:25 +0300

    delete methods in FilesPagination

--- src/fileservice/pagination.py
+++ src/fileservice/pagination.py
@@ -1,5 +1,4 @@
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
 
 
 class FilesPagination(PageNumberPagination):
@@ -7,12 +6,3 @@ class FilesPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
-    # def get_paginated_response(self, data):
-    #     return Response({
-    #         'links': {
-    #             'next': self.get_next_link(),
-    #             'previous': self.get_previous_link()
-    #         },
-    #         'count': self.page.paginator.count,
-    #         'results': data
-    #     })
