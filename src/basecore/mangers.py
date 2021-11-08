841107db1dd3b5b2ef9a13ce3ef4958156c5f8ca
Your Name <you@example.com>
2021-11-08 15:35:26 +0300

    fix M in manager

--- src/basecore/mangers.py
+++ src/basecore/mangers.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import QuerySet
 
 
-class BaseModelManager(models.manager):
+class BaseModelManager(models.Manager):
 
     def get_queryset(self) -> QuerySet:
         return super().get_queryset().filter(is_alive=True)
