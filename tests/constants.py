8d3eaa7c1c700e7696f222de3fcd041b6a02aa78
andrey <1234567>
2021-11-10 17:32:06 +0300

    create docker-compouse-test for testing contailner with pytest

--- tests/constants.py
+++ tests/constants.py
@@ -1,5 +1,18 @@
+TEST_USERNAME = 'test_username'
+TEST_EMAIL = 'test_email@gmail.com'
+TEST_PASSWORD = 'test_password'
+
 TEST_USER_REGISTER_DATA = {'first_name': 'test_first_name',
                            'last_name': 'test_last_name',
-                           'email': 'test_email@gmail.com',
-                           'password': 'test_password',
-                           'username': 'test_username'}
+                           'email': TEST_EMAIL,
+                           'password': TEST_PASSWORD,
+                           'username': TEST_USERNAME,
+                           }
+
+TEST_FILE_NAME = 'test_file'
+TEST_FILE_TYPE = '.txt'
+TEST_FILE_PATH = f'tests/{TEST_FILE_NAME}{TEST_FILE_TYPE}'
+TEST_FILE_DATA = {'filename': TEST_FILE_NAME,
+                  'type': TEST_FILE_TYPE,
+                  'total_size': 48
+                  }
