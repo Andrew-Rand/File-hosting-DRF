47e97c336f42d852a072801e912e6a72f2fb9abd
andrey <1234567>
2021-11-10 19:56:30 +0300

    fix auth and non auth test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,30 +1,33 @@
 import os
+from typing import Tuple
 
 import pytest
+from rest_framework.test import APIClient
 
 from src.accounts.authentication import create_token
 from src.accounts.constants import ACCESS_TOKEN_LIFETIME
 from src.accounts.models import User
+from src.fileservice.models import File
 from src.fileservice.utils import calculate_hash_md5
 from tests.constants import TEST_USERNAME, TEST_EMAIL, TEST_PASSWORD, TEST_STORAGE_PATH, TEST_FILE_DATA, TEST_FILE_NAME, \
     TEST_FILE_TYPE
 
 
 @pytest.fixture
-def test_client():
+def test_client() -> APIClient:
     from rest_framework.test import APIClient
     return APIClient()
 
 
 @pytest.fixture
-def create_user_and_get_token():
+def create_user_and_get_token() -> Tuple[User, str]:
     user = User.objects.create_user(username=TEST_USERNAME, email=TEST_EMAIL, password=TEST_PASSWORD)
     token = create_token(str(user.id), time_delta_seconds=ACCESS_TOKEN_LIFETIME)
     return user, token
 
 
 @pytest.fixture
-def file_create(create_user_and_get_token):
+def file_create(create_user_and_get_token) -> File:
     from src.fileservice.models import File
     from src.fileservice.models import FileStorage
     from src.fileservice.models.file_storage import PERMANENT_STORAGE
