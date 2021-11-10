06a668d7f42b0eb75873566461e24d2fff746193
Your Name <you@example.com>
2021-11-10 14:44:06 +0300

    create constant url names, create test for user register, create test for unauth

---
+++ tests/test_database.py
@@ -0,0 +1,17 @@
+import pytest
+
+from django.contrib.auth import get_user_model
+
+
+@pytest.mark.django_db
+def test_user_create():
+
+    User = get_user_model()
+    # user_data = {'first_name': 'test_name',
+    #              'last_name': 'test_lname',
+    #              'email': 'test_email@gmail.ru',
+    #              'password': '1111',
+    #              'username': 'aaaa'}
+
+    User.objects.create()
+    assert User.objects.count() == 1
