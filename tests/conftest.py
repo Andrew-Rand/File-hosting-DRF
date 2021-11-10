436db819c2a6b0386fa3ddccf87ed6042f89d9fc
andrey <1234567>
2021-11-10 19:38:42 +0300

    create test authorized views

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
+import os
+
 import pytest
 
 from src.accounts.authentication import create_token
 from src.accounts.constants import ACCESS_TOKEN_LIFETIME
 from src.accounts.models import User
 from src.fileservice.utils import calculate_hash_md5
-from tests.constants import TEST_USERNAME, TEST_EMAIL, TEST_PASSWORD, TEST_FILE_PATH, TEST_FILE_DATA
+from tests.constants import TEST_USERNAME, TEST_EMAIL, TEST_PASSWORD, TEST_STORAGE_PATH, TEST_FILE_DATA, TEST_FILE_NAME, \
+    TEST_FILE_TYPE
 
 
 @pytest.fixture
@@ -14,31 +17,33 @@ def test_client():
 
 
 @pytest.fixture
-def create_user_with_token():
+def create_user_and_get_token():
     user = User.objects.create_user(username=TEST_USERNAME, email=TEST_EMAIL, password=TEST_PASSWORD)
     token = create_token(str(user.id), time_delta_seconds=ACCESS_TOKEN_LIFETIME)
     return user, token
 
 
 @pytest.fixture
-def get_file_hash():
-    test_file = open(TEST_FILE_PATH, 'w')
-    test_file.close()
-    file_hash = calculate_hash_md5(TEST_FILE_PATH)
-    return file_hash
-
-
-@pytest.fixture
-def file_create(create_user_with_token, get_file_hash):
+def file_create(create_user_and_get_token):
     from src.fileservice.models import File
     from src.fileservice.models import FileStorage
     from src.fileservice.models.file_storage import PERMANENT_STORAGE
 
     permanent_storage = FileStorage.objects.get(type=PERMANENT_STORAGE)
-    File.create_model_object(user=create_user_with_token[0],
-                                    hash=get_file_hash,
-                                    storage=permanent_storage,
-                                    destination=TEST_FILE_PATH,
-                                    data=TEST_FILE_DATA)
-    file = File.objects.filter(user=create_user_with_token[0]).first()
+    user = create_user_and_get_token[0]
+    os.mkdir(f'storage/permanent/{user.id}')
+    test_file_path = f'{TEST_STORAGE_PATH}/{user.id}/{TEST_FILE_NAME}{TEST_FILE_TYPE}'
+    test_file = open(test_file_path, 'w')
+    test_file.close()
+
+    file_hash = calculate_hash_md5(test_file_path)
+
+    File.create_model_object(user=user,
+                             hash=file_hash,
+                             storage=permanent_storage,
+                             destination=f'{user.id}/{TEST_FILE_NAME}{TEST_FILE_TYPE}',
+                             data=TEST_FILE_DATA)
+
+    file = File.objects.filter(user=create_user_and_get_token[0]).first()
+
     return file
--- tests/test_unauthorized_view.py
+++ tests/test_unauthorized_view.py
@@ -2,37 +2,76 @@ import pytest
 
 from django.urls import reverse
 
-from src.fileservice.constants import FILE_DETAIL_URL_NAME
+from src.accounts.constants import ACCOUNTS_DETAIL_URL_NAME
+from src.fileservice.constants import FILE_DETAIL_URL_NAME, FILE_DOWNLOAD_URL_NAME, FILE_UPLOAD_URL_NAME, \
+    FILE_BUILD_URL_NAME, FILE_ALL_USER_FILES_URL_NAME, FILE_DOWNLOAD_ALL_URL_NAME, FILE_CHUNK_UPLOAD_URL_NAME
+from tests.constants import TEST_QUERYSET_FOR_BUILD
 
 
 class TestUnauthorizedRequest:
 
     @pytest.mark.django_db
-    def test_unauthorized_user_detail(self, test_client):
-        url = reverse('user_detail')
+    def test_unauthorized_user_detail(self, test_client) -> None:
+        url = reverse(ACCOUNTS_DETAIL_URL_NAME)
         response = test_client.get(url)
         assert response.status_code == 403
 
     @pytest.mark.django_db
-    def test_unauthorized_chunk_upload(self, test_client):
-        url = reverse('chunk_upload')
+    def test_unauthorized_chunk_upload(self, test_client) -> None:
+        url = reverse(FILE_CHUNK_UPLOAD_URL_NAME)
         response = test_client.get(url)
         assert response.status_code == 403
 
     @pytest.mark.django_db
-    def test_unauthorized_download_all_file_as_zip(self, test_client):
-        url = reverse('download_all_file_as_zip')
+    def test_unauthorized_chunk_upload(self, test_client) -> None:
+        url = reverse(FILE_CHUNK_UPLOAD_URL_NAME)
+        response = test_client.post(url)
+        assert response.status_code == 403
+
+    @pytest.mark.django_db
+    def test_unauthorized_download_all_file_as_zip(self, test_client) -> None:
+        url = reverse(FILE_DOWNLOAD_ALL_URL_NAME)
         response = test_client.get(url)
         assert response.status_code == 403
 
     @pytest.mark.django_db
-    def test_unauthorized_all_user_files(self, test_client):
-        url = reverse('all_user_files')
+    def test_unauthorized_all_user_files(self, test_client) -> None:
+        url = reverse(FILE_ALL_USER_FILES_URL_NAME)
         response = test_client.get(url)
         assert response.status_code == 403
 
     @pytest.mark.django_db
-    def test_unauthorized_file_detail(self, test_client, file_create):
+    def test_unauthorized_file_detail_get(self, test_client, file_create) -> None:
         url = reverse(FILE_DETAIL_URL_NAME, kwargs={'pk': str(file_create.id)})
         response = test_client.get(url)
+        assert response.status_code == 403
+
+    @pytest.mark.django_db
+    def test_unauthorized_file_detail_patch(self, test_client, file_create) -> None:
+        url = reverse(FILE_DETAIL_URL_NAME, kwargs={'pk': str(file_create.id)})
+        response = test_client.patch(url)
+        assert response.status_code == 403
+
+    @pytest.mark.django_db
+    def test_unauthorized_file_detail_delete(self, test_client, file_create) -> None:
+        url = reverse(FILE_DETAIL_URL_NAME, kwargs={'pk': str(file_create.id)})
+        response = test_client.get(url)
+        assert response.status_code == 403
+
+    @pytest.mark.django_db
+    def test_unauthorized_file_download(self, test_client, file_create) -> None:
+        url = reverse(FILE_DOWNLOAD_URL_NAME, kwargs={'pk': str(file_create.id)})
+        response = test_client.get(url)
+        assert response.status_code == 403
+
+    @pytest.mark.django_db
+    def test_unauthorized_file_upload(self, test_client, file_create) -> None:
+        url = reverse(FILE_UPLOAD_URL_NAME)
+        response = test_client.post(url)
+        assert response.status_code == 403
+
+    @pytest.mark.django_db
+    def test_unauthorized_file_build(self, test_client, file_create) -> None:
+        url = reverse(FILE_BUILD_URL_NAME)
+        response = test_client.post(url + TEST_QUERYSET_FOR_BUILD)
         assert response.status_code == 403
