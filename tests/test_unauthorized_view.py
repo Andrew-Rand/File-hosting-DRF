140c620c3bc7867c1357751c52bddff78351164d
andrey <1234567>
2021-11-10 17:32:06 +0300

    create docker-compouse-test for testing contailner with pytest

--- tests/test_unauthorized_view.py
+++ tests/test_unauthorized_view.py
@@ -2,6 +2,8 @@ import pytest
 
 from django.urls import reverse
 
+from src.fileservice.constants import FILE_DETAIL_URL_NAME
+
 
 class TestUnauthorizedRequest:
 
@@ -26,5 +28,11 @@ class TestUnauthorizedRequest:
     @pytest.mark.django_db
     def test_unauthorized_all_user_files(self, test_client):
         url = reverse('all_user_files')
+        response = test_client.get(url)
+        assert response.status_code == 403
+
+    @pytest.mark.django_db
+    def test_unauthorized_file_detail(self, test_client, file_create):
+        url = reverse(FILE_DETAIL_URL_NAME, kwargs={'pk': str(file_create.id)})
         response = test_client.get(url)
         assert response.status_code == 403
