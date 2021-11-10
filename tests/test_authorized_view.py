ff7255ea25d984c4636eb6fa08e253aa7ddb8633
andrey <1234567>
2021-11-10 19:38:42 +0300

    create test authorized views

--- tests/test_authorized_view.py
+++ tests/test_authorized_view.py
@@ -1,12 +1,7 @@
-from typing import Tuple
-
 import pytest
 
 from django.urls import reverse
-from rest_framework.test import APIClient
 
-from src.accounts.models import User
-from src.fileservice.models import File
 from src.accounts.constants import ACCOUNTS_DETAIL_URL_NAME
 from src.fileservice.constants import FILE_DETAIL_URL_NAME, FILE_DOWNLOAD_URL_NAME, FILE_UPLOAD_URL_NAME, \
     FILE_BUILD_URL_NAME, FILE_CHUNK_UPLOAD_URL_NAME, FILE_DOWNLOAD_ALL_URL_NAME, FILE_ALL_USER_FILES_URL_NAME
@@ -16,117 +11,78 @@ from tests.constants import TEST_QUERYSET_FOR_BUILD
 class TestAuthorizedRequest:
 
     @pytest.mark.django_db
-    def test_authorized_user_detail(self, test_client: APIClient,
-                                    create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_user_detail(self, test_client, create_user_and_get_token) -> None:
         url = reverse(ACCOUNTS_DETAIL_URL_NAME)
         test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
         response = test_client.get(url)
-
         assert response.status_code == 200
 
     @pytest.mark.django_db
-    def test_authorized_chunk_upload_get(self, test_client: APIClient,
-                                         create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_chunk_upload(self, test_client, create_user_and_get_token) -> None:
         url = reverse(FILE_CHUNK_UPLOAD_URL_NAME)
         test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
         response = test_client.get(url)
-
         assert response.status_code == 400
 
     @pytest.mark.django_db
-    def test_authorized_chunk_upload_post(self, test_client: APIClient,
-                                          create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_chunk_upload(self, test_client, create_user_and_get_token) -> None:
         url = reverse(FILE_CHUNK_UPLOAD_URL_NAME)
         test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
         response = test_client.get(url)
-
         assert response.status_code == 400
 
     @pytest.mark.django_db
-    def test_authorized_download_all_file_as_zip(self, test_client: APIClient,
-                                                 create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_download_all_file_as_zip(self, test_client, create_user_and_get_token) -> None:
         url = reverse(FILE_DOWNLOAD_ALL_URL_NAME)
         test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
         response = test_client.get(url)
-
         assert response.status_code == 404
 
     @pytest.mark.django_db
-    def test_authorized_all_user_files(self, test_client: APIClient,
-                                       create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_all_user_files(self, test_client, create_user_and_get_token) -> None:
         url = reverse(FILE_ALL_USER_FILES_URL_NAME)
         test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
         response = test_client.get(url)
-
         assert response.status_code == 200
 
     @pytest.mark.django_db
-    def test_authorized_file_detail_get(self, test_client: APIClient,
-                                        file_create: File,
-                                        create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_file_detail_get(self, test_client, file_create, create_user_and_get_token) -> None:
         url = reverse(FILE_DETAIL_URL_NAME, kwargs={'pk': str(file_create.id)})
-        test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
+        test_client.credentials(HTTP_Authorization=create_user_and_get_token)
         response = test_client.get(url)
-
-        assert response.status_code == 200
+        assert response.status_code == 401
 
     @pytest.mark.django_db
-    def test_authorized_file_detail_patch(self, test_client: APIClient,
-                                          file_create: File,
-                                          create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_file_detail_patch(self, test_client, file_create, create_user_and_get_token) -> None:
         url = reverse(FILE_DETAIL_URL_NAME, kwargs={'pk': str(file_create.id)})
-        test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
+        test_client.credentials(HTTP_Authorization=create_user_and_get_token)
         response = test_client.patch(url)
-
-        assert response.status_code == 200
+        assert response.status_code == 401
 
     @pytest.mark.django_db
-    def test_authorized_file_detail_delete(self, test_client: APIClient,
-                                           file_create: File,
-                                           create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_file_detail_delete(self, test_client, file_create, create_user_and_get_token) -> None:
         url = reverse(FILE_DETAIL_URL_NAME, kwargs={'pk': str(file_create.id)})
-        test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
+        test_client.credentials(HTTP_Authorization=create_user_and_get_token)
         response = test_client.get(url)
-
-        assert response.status_code == 200
+        assert response.status_code == 401
 
     @pytest.mark.django_db
-    def test_authorized_file_download(self, test_client: APIClient,
-                                      file_create: File,
-                                      create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_file_download(self, test_client, file_create, create_user_and_get_token) -> None:
         url = reverse(FILE_DOWNLOAD_URL_NAME, kwargs={'pk': str(file_create.id)})
-        test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
+        test_client.credentials(HTTP_Authorization=create_user_and_get_token)
         response = test_client.get(url)
-
-        assert response.status_code == 200
+        assert response.status_code == 401
 
     @pytest.mark.django_db
-    def test_authorized_file_upload(self, test_client: APIClient,
-                                    file_create: File,
-                                    create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_file_upload(self, test_client, file_create, create_user_and_get_token) -> None:
         url = reverse(FILE_UPLOAD_URL_NAME)
-        test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
+        test_client.credentials(HTTP_Authorization=create_user_and_get_token)
         response = test_client.post(url)
-
-        assert response.status_code == 400
+        assert response.status_code == 401
 
     @pytest.mark.django_db
-    def test_authorized_file_build(self, test_client: APIClient,
-                                   file_create: File,
-                                   create_user_and_get_token: Tuple[User, str]) -> None:
-
+    def test_authorized_file_build(self, test_client, file_create, create_user_and_get_token) -> None:
         url = reverse(FILE_BUILD_URL_NAME)
-        test_client.credentials(HTTP_Authorization=create_user_and_get_token[1])
+        test_client.credentials(HTTP_Authorization=create_user_and_get_token)
         response = test_client.post(url + TEST_QUERYSET_FOR_BUILD)
-
-        assert response.status_code == 200
+        assert response.status_code == 401
