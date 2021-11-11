dc5d0413f50c3836742f4194e0d65299788d2439
Your Name <you@example.com>
2021-11-11 09:34:46 +0300

    Add tests for filebuild and fix auth tests

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -11,14 +11,18 @@ class TestUserRegister:
 
     @pytest.mark.django_db
     def test_register_view_ok(self, test_client: APIClient) -> None:
+
         url = reverse(ACCOUNTS_REGISTER_URL_NAME)
         data = TEST_USER_REGISTER_DATA
         response = test_client.post(url, data=data)
+
         assert response.status_code == 201
 
     @pytest.mark.django_db
     def test_register_view_bad(self, test_client: APIClient) -> None:
+
         url = reverse(ACCOUNTS_REGISTER_URL_NAME)
         data = {}
         response = test_client.post(url, data=data)
+
         assert response.status_code == 400
