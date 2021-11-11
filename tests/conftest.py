53382ad794ff4b8ead456ebae89db382ded19494
Your Name <you@example.com>
2021-11-11 09:34:46 +0300

    Add tests for filebuild and fix auth tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,8 +9,8 @@ from src.accounts.constants import ACCESS_TOKEN_LIFETIME
 from src.accounts.models import User
 from src.fileservice.models import File
 from src.fileservice.utils import calculate_hash_md5
-from tests.constants import TEST_USERNAME, TEST_EMAIL, TEST_PASSWORD, TEST_STORAGE_PATH, TEST_FILE_DATA, TEST_FILE_NAME, \
-    TEST_FILE_TYPE
+from tests.constants import TEST_USERNAME, TEST_EMAIL, TEST_PASSWORD, TEST_STORAGE_PATH, TEST_FILE_DATA,\
+    TEST_FILE_NAME, TEST_FILE_TYPE, TEST_CHUNK_PATH, TEST_CHUNK_NAME, TEST_QUERYSET_FOR_BUILD
 
 
 @pytest.fixture
@@ -27,14 +27,14 @@ def create_user_and_get_token() -> Tuple[User, str]:
 
 
 @pytest.fixture
-def file_create(create_user_and_get_token) -> File:
+def file_create(create_user_and_get_token: Tuple[User, str]) -> File:
     from src.fileservice.models import File
     from src.fileservice.models import FileStorage
     from src.fileservice.models.file_storage import PERMANENT_STORAGE
 
     permanent_storage = FileStorage.objects.get(type=PERMANENT_STORAGE)
     user = create_user_and_get_token[0]
-    os.mkdir(f'storage/permanent/{user.id}')
+    os.mkdir(f'{TEST_STORAGE_PATH}/{user.id}')
     test_file_path = f'{TEST_STORAGE_PATH}/{user.id}/{TEST_FILE_NAME}{TEST_FILE_TYPE}'
     test_file = open(test_file_path, 'w')
     test_file.close()
@@ -50,3 +50,14 @@ def file_create(create_user_and_get_token) -> File:
     file = File.objects.filter(user=create_user_and_get_token[0]).first()
 
     return file
+
+
+@pytest.fixture
+def chunks_create(create_user_and_get_token) -> str:
+    user = create_user_and_get_token[0]
+    user_chunks_path = f'{TEST_CHUNK_PATH}/{user.id}/148-test_chunktxt/'
+    os.makedirs(user_chunks_path, 0o777, exist_ok=True)
+    test_file_path = f'{user_chunks_path}{TEST_CHUNK_NAME}'
+    test_chunk = open(test_file_path, 'w')
+    test_chunk.close()
+    return TEST_QUERYSET_FOR_BUILD
