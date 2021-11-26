193787af2429e1ca7f36bc8cdc39104ea9afa6a2
Your Name <you@example.com>
2021-11-26 14:07:58 +0300

    add rm in deploy sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,14 +12,19 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 CWD="$(pwd)"
 
+# Lint project before deploy
 ./scripts/linters.sh
 ./scripts/tests.sh
 
+#  Kill remote project
+ssh root@64.227.117.166 "cd /home/andrew && docker-compose down"
+ssh root@64.227.117.166 "cd /home/andrew && sudo rm -r src/ srv/ static/ .env docker-compose.yml entrypoint.sh manage.py Pipfile Pipfile.lock"
+
+# Deploy new version
 tar -zcf archive_to_deploy.tar.gz src/ srv/ static/ .env docker-compose.yml entrypoint.sh manage.py Pipfile Pipfile.lock
-
-
 scp archive_to_deploy.tar.gz root@64.227.117.166:/home/andrew
-
 ssh root@64.227.117.166 "cd /home/andrew && tar -xzf archive_to_deploy.tar.gz"
+
+# Run new version
 ssh root@64.227.117.166 "cd /home/andrew && docker-compose build"
 ssh root@64.227.117.166 "cd /home/andrew && docker-compose up"
